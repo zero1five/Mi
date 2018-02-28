3e13f439bd2ca6cdfba8a8a0ba24e66bdd5724e3
zero1five <2642585002@qq.com>
2018-03-01 05:24:10 +0800

    Smartisan - mall 最终版

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <div id="app">
-  	<header-nav></header-nav>	
+  	<header-nav></header-nav>
   	<router-view></router-view>
+    <footer-site></footer-site>
   </div>
 </template>
 
 <script>
 
 import headerNav from '@/components/header-nav'
-
+import footerSite from '@/components/footer-site'
 export default {
   name: 'app',
   components: {
-    headerNav
+    headerNav,
+    footerSite
   }
 }
 </script>
---
+++ src/components/footer-site.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="app-foot">
+      <p>Copyright © , Smartisan Digital Co., Ltd. All Rights Reserved.北京锤子数码科技有限公司</p>
+    </div>
+  </div>
+</template>
+<script>
+</script>
+<style>
+.app-foot {
+  text-align: center;
+  height: 80px;
+  width: 100%;
+  line-height: 80px;
+  background: #e3e4e8;
+  clear: both;
+  position: fixed;
+  bottom: 0;
+}
+</style>
